0c799c34c6cd2176e4633b9896231a844b66d5e4
fireflyoc <fireflyoc@gmail.com>
2018-06-27 09:00:38 -0400

    Added pokedex functionalality.

--- team-rocket-frontend/src/app/app.module.ts
+++ team-rocket-frontend/src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PokedexComponent } from './components/pokedex/pokedex.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { PokemonService } from './components/pokemon/pokemon.service';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PokemonCardComponent } from './components/pokcard/pokcard.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexComponent,
+    PokemonComponent,
+    PokemonCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
