09bb23575c1d4fdc36f21ac66957dd03143f0478
Nick Stubbs <nickdstubbs@gmail.com>
2018-06-30 21:56:28 -0400

    Putting back in folder

---
+++ PokeTeam/src/main/java/com/poketeam/BootDriver.java
@@ -0,0 +1,12 @@
+package com.poketeam;
+
+import org.springframework.boot.SpringApplication;
+
+public class BootDriver {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDriver.class, args);
+
+	}
+
+}
