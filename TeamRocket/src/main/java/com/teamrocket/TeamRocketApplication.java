81d742ad5d8e230248fd7a8b5075d09956cd503d
Nick Stubbs <nickdstubbs@gmail.com>
2018-07-02 11:21:18 -0400

    Change to war

--- TeamRocket/src/main/java/com/teamrocket/TeamRocketApplication.java
+++ TeamRocket/src/main/java/com/teamrocket/TeamRocketApplication.java
@@ -2,10 +2,16 @@ package com.teamrocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TeamRocketApplication {
+public class TeamRocketApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TeamRocketApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(TeamRocketApplication.class, args);
 	}
